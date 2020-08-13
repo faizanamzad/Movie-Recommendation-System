81f5f864ceb5a711a7e1db789088a920fd5c4e26
faizanamzad <faizan.amzad@gmail.com>
2020-08-13 14:20:58 +0530

    pulled branch

--- MovieRecommendationSystem.py
+++ MovieRecommendationSystem.py
@@ -5,6 +5,9 @@ Created on Sun Jan 13 15:59:17 2019
 @author: Faizan
 """
 
+
+#pulled branch
+
 import numpy as np
 import pandas as pd
 #pd.set_option('display.expand_frame_repr', False)
@@ -14,6 +17,8 @@ import pandas as pd
 #pd.set_option('display.width', 1000)
 ratings_data = pd.read_csv("ml-latest-small/ratings.csv")
 #print(ratings_data.head())
+
+
 
 movie_names = pd.read_csv("ml-latest-small/movies.csv")
 #print(movie_names.head())
